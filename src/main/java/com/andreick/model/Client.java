f9900368402df71b40518f2fb4f562f2bfed6b43
Andreick <andreick.gomes.ismart@gmail.com>
2022-12-24 19:50:43 -0300

    Using embeddable

--- src/main/java/com/andreick/model/Client.java
+++ src/main/java/com/andreick/model/Client.java
@@ -9,38 +9,26 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String cpf;
+
+    @Embedded
+    private PersonalData personalData = new PersonalData();
 
     public Client() {
     }
 
     public Client(String name, String cpf) {
-        this.name = name;
-        this.cpf = cpf;
+        personalData = new PersonalData(name, cpf);
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        return personalData.getName();
     }
 
     public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+        return personalData.getCpf();
     }
 }
